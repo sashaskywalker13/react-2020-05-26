f4e6285d9f766533d18c7c221d873aa169578379
Andrii Koretskyi <andriiko@wix.com>
2020-06-02 20:57:03 +0300

    add componentDidCatch

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -12,8 +12,21 @@ class Menu extends React.Component {
     ).isRequired,
   };
 
+  state = {
+    error: null,
+  };
+
+  componentDidCatch(error) {
+    this.setState({ error });
+  }
+
   render() {
     const { menu } = this.props;
+
+    if (this.state.error) {
+      return <h1>{this.state.error.message}</h1>;
+    }
+
     return (
       <div className={styles.menu}>
         <div>
