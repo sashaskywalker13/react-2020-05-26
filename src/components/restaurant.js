fdbfee50b142c272ed344d9acbdf2d80b0facd9d
Александр Мухин <aleksandrmuhin@MacBook-Pro-Aleksandr.local>
2020-06-03 12:29:12 +0300

    add useMemo

--- src/components/restaurant.js
+++ src/components/restaurant.js
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 
 import Reviews from './reviews';
 import Menu from './menu';
 import Rate from './rate';
 
 export default function Restaurant(props) {
-  const activeRating = props.restaurant.reviews.map(
-    (restaurant) => restaurant.rating
+  const activeRating = useMemo(
+    () => props.restaurant.reviews.map((restaurant) => restaurant.rating),
+    [props.restaurant.reviews]
   );
-  const averageRating = (
-    activeRating.reduce((sum, current) => sum + current, 0) /
-    activeRating.length
-  ).toFixed(1);
+  const averageRating = useMemo(
+    () =>
+      (
+        activeRating.reduce((sum, current) => sum + current, 0) /
+        activeRating.length
+      ).toFixed(1),
+    [activeRating]
+  );
   return (
     <div>
       <Menu menu={props.restaurant.menu} />
