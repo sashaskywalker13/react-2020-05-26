b431f5055fa9a07e5f73812bf085897dee76683b
Andrii Koretskyi <andriiko@wix.com>
2020-06-12 21:15:07 +0300

    add 3 actions per reauest

--- src/redux/middleware/api.js
+++ src/redux/middleware/api.js
@@ -1,9 +1,16 @@
+import { SUCCESS, REQUEST, FAILURE } from '../constants';
+
 export default (store) => (next) => async (action) => {
   if (!action.CallAPI) return next(action);
 
-  const { CallAPI, ...rest } = action;
+  const { CallAPI, type, ...rest } = action;
 
-  const response = await fetch(CallAPI).then((res) => res.json());
+  next({ ...rest, type: type + REQUEST });
 
-  next({ ...rest, response });
+  try {
+    const response = await fetch(CallAPI).then((res) => res.json());
+    next({ ...rest, type: type + SUCCESS, response });
+  } catch (error) {
+    next({ ...rest, type: type + FAILURE, error });
+  }
 };
