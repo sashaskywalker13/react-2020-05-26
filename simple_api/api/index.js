bec3a7bb9fe1562c12ac31888be82d7ae7cc38f8
Andrii Koretskyi <andriiko@wix.com>
2020-06-19 19:57:09 +0300

    add order api

--- simple_api/api/index.js
+++ simple_api/api/index.js
@@ -50,4 +50,29 @@ router.get('/users', function (req, res, next) {
   reply(res, mocks.users);
 });
 
+router.post('/order', function (req, res, next) {
+  const total = req.body
+    .map(
+      ({ id, amount }) =>
+        mocks.products.find((product) => product.id === id).price * amount
+    )
+    .reduce((acc, next) => acc + next, 0);
+
+  if (total < 50)
+    return reply(
+      res,
+      `you ordered for $${total}, but the minimum order amount is $50`,
+      3000,
+      400
+    );
+  if (total > 200)
+    return reply(
+      res,
+      `you ordered for $${total}, but the maximum order amount is $200`,
+      3000,
+      400
+    );
+  return reply(res, 'ok', 3000);
+});
+
 module.exports = router;
